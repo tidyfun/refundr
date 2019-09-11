d799e69f462ffdba2d37a2dfb2de881333382b8d
Julia Wrobel <juliawrobel@Xs-iMac-Pro.local>
2019-09-11 11:12:27 -0600

    added unit tests for tidy_fpca()

---
+++ tests/testthat/test-tidy_fpca.R
@@ -0,0 +1,15 @@
+context("fpca")
+library(refunder)
+library(tidyfun)
+
+test_that("tidy_fpca runs on dti data", {
+  data(dti_df)
+  expect_error(tidy_fpca(cca, dti_df), NA)
+})
+
+
+
+test_that("multiple do-dot-dot arguments accepted for fpca_sc", {
+  data(dti_df)
+  expect_error(tidy_fpca(cca, dti_df, nbasis = 9, makePD = TRUE), NA)
+})
