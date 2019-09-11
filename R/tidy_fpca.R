5de97b99a2f14b0bb14a64d626f2947ea1eedb30
Julia Wrobel <juliawrobel@Xs-iMac-Pro.local>
2019-09-11 08:21:33 -0600

    created tidy_fpca function

---
+++ R/tidy_fpca.R
@@ -0,0 +1,21 @@
+##' Tidy interface to refund fpca functions
+##'
+##' Allows calls to \code{fpca.sc}, \code{fpca.face}, and \code{fpca.ssvd} as implemented in the \code{refund} package. Tidy functional data objects are input and tidy objects are returned as well.
+##'
+##'
+##' @param Y a \code{tfd} vector of data (regular vs. irregular may be assigned differnet defaults).
+##' @param data a \code{tidyfun} dataframe containing a \code{tfd} vector of data.
+##' @param argvals
+##' @return
+##'
+##' @author Julia, Jeff, Fabian (plus any authors of the original refund code)
+##'
+##' @references keep original references
+##'
+##' @examples
+##' \dontrun{
+##' }
+
+tidy_fpca <- function(Y, data, argvals = NULL, ...){
+
+}
