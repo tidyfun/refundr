d3f62a23008f6c8c8a7aefda91aaa9556a5890e3
Fabian Scheipl <fabian.scheipl@googlemail.com>
2019-10-10 16:39:13 +0200

    remove copy-paste-modify docs for rfr_fpca-methods
    merge into one man-page, fix formatting & cross-refs

--- R/rfr_fpca.tfd_irreg.R
+++ R/rfr_fpca.tfd_irreg.R
@@ -1,29 +1,4 @@
-##' Tidy interface for fpca with irregularly-spaced functional data
-##'
-##' Default call for irregular data is to `fpca_sc`, this function processes the data and provides a wrapper to `fpca_sc`.
-##'
-##'
-##' @param data a `tfd` data vector.
-##' @param pve proportion of variance explained: used to choose the number of
-##' principal components.
-##' @param npc prespecified value for the number of principal components (if
-##' given, this overrides `pve`).
-##' @param method fpca method of choice. Different options are available but defaults will be set depending on regularity/irregularity of the data
-##' @param ... optional arguments to be passed to methods. Takes arguments from original `refund` functions.
-##' @return
-##'
-##' @author Julia, Jeff, Fabian (plus any authors of the original refund code)
-##' @seealso [rfr_fpca()], [rfr_fpca.tfd_reg()]
-##'
-##' @examples
-##' \dontrun{
-##' library(refunder)
-##' data(dti_df)
-##' fpca_results <- rfr_fpca(data = dti_df$cca)
-##' }
-##'
-##' @importFrom tidyr spread
-##' @import tidyfun
+##' @describeIn rfr_fpca FPCA for data on irregular grids defaults to [refund::fpca.sc()]
 ##' @export
 rfr_fpca.tfd_irreg <- function(data, pve = 0.99, npc = NULL, method = fpca_sc, ...){
 
--- R/rfr_fpca.tfd_reg.R
+++ R/rfr_fpca.tfd_reg.R
@@ -1,28 +1,4 @@
-##' Tidy interface for fpca with regularly-spaced functional data
-##'
-##' Default call for regular data is to `fpca_face`, this function processes the data and provides a wrapper to `fpca_face`.
-##'
-##'
-##' @param data a `tfd` data vector.
-##' @param pve proportion of variance explained: used to choose the number of
-##' principal components.
-##' @param npc prespecified value for the number of principal components (if
-##' given, this overrides `pve`).
-##' @param method fpca method of choice. Different options are available but defaults will be set depending on regularity/irregularity of the data
-##' @param ... optional arguments to be passed to methods. Takes arguments from original `refund` functions.
-##' @return
-##'
-##' @author Julia, Jeff, Fabian (plus any authors of the original refund code)
-##' @seealso [rfr_fpca()], [rfr_fpca.tfd_irreg()]
-##'
-##' @examples
-##' \dontrun{
-##' library(refunder)
-##' data(chf_df)
-##' fpca_results <- rfr_fpca(data = chf_df$activity)
-##' }
-##' @importFrom tidyr spread
-##' @importFrom splines spline.des
+##' @describeIn rfr_fpca FPCA for data on a regular grid defaults to [refund::fpca.face()]
 ##' @export
 rfr_fpca.tfd_reg <- function(data, pve = 0.99, npc = NULL, method = fpca_face, ...){
 
