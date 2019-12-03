a5fddeee032fc22180a370a82a431e3ac5ee7172
Julia Wrobel <julia.wrobel@cuanschutz.edu>
2019-12-03 15:32:04 -0700

    edited readme

--- R/fpca-methods.R
+++ R/fpca-methods.R
@@ -13,7 +13,7 @@
 ##' @method residuals rfr_fpca
 ##' @aliases fitted.rfr_fpca
 ##' @export
-residuals.rfr_fpca = function(object, ...){
+residuals.rfr_fpca <- function(object, ...){
   object$Y - tfd(object$Yhat_tfb, arg = tf_arg(object$Y))
 }
 
@@ -21,7 +21,7 @@ residuals.rfr_fpca = function(object, ...){
 ##' @method fitted rfr_fpca
 ##' @export
 ##' @rdname residuals.rfr_fpca
-fitted.rfr_fpca = function(object, ...){
+fitted.rfr_fpca <- function(object, ...){
   tfd(object$Yhat_tfb, arg = tf_arg(object$Y))
 }
 
@@ -32,11 +32,19 @@ fitted.rfr_fpca = function(object, ...){
 #' model fit.
 #'
 ##' @param object an \code{rf_fpca} object
+##' @param newdata An optional `tfd` data vector. If omitted, the fitted values are returned.
 ##' @param ... optional arguments to be passed to methods
 ##'
 ##' @importFrom stats fitted fitted.values
 ##' @method predict rfr_fpca
 ##' @export
-predict.rfr_fpca = function(object, ...){
-  tfd(object$Yhat_tfb)
+predict.rfr_fpca <- function(object, newdata, ...){
+  ## need different behavior for regular vs. irregular objects?
+   # nah, just need to make sure it works for irregular objects.
+    # default behavior (with no new data) is to return fitted values
+  if(missing(newdata) || is.null(newdata)) {
+    fitted(object)
+  }
+
+
 }
