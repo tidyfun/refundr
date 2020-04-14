452747ce73145ab21617d140a54f8d4c585c586b
Jeff Goldsmith <jeff.goldsmith@columbia.edu>
2020-04-14 16:09:25 -0400

    include name of fpca method in object -- probably want for print method anyway

--- R/rfr_fpca.tfd_irreg.R
+++ R/rfr_fpca.tfd_irreg.R
@@ -2,9 +2,15 @@
 ##' @export
 rfr_fpca.tfd_irreg <- function(Y, data, pve = 0.99, npc = NULL, method = fpca_sc, ...){
 
-  Y <- data[[Y]]
-  results <- tfb_fpc(Y, method = method, pve = pve, npc = npc, ...)
+  method_name = rlang::as_name(enquo(method))
+
+  tf_vec <- data[[Y]]
+  results <- tfb_fpc(tf_vec, method = method, pve = pve, npc = npc, ...)
+
   results_ls <- extract_fpca(results)
-  results_ls$Y <- Y
+  results_ls$Y <- tf_vec
+  results_ls$model_var <- Y
+  results_ls$method = method_name
+
   return(results_ls)
 }
--- R/rfr_fpca.tfd_reg.R
+++ R/rfr_fpca.tfd_reg.R
@@ -2,10 +2,15 @@
 ##' @export
 rfr_fpca.tfd_reg <- function(Y, data, pve = 0.99, npc = NULL, method = fpca_face, ...){
 
-  Y <- data[[Y]]
-  results <- tfb_fpc(Y, method = method, pve = pve, npc = npc, ...)
-  results_ls <- extract_fpca(results)
-  results_ls$Y <- Y
-  return(results_ls)
+  method_name = rlang::as_name(enquo(method))
 
+  tf_vec <- data[[Y]]
+  results <- tfb_fpc(tf_vec, method = method, pve = pve, npc = npc, ...)
+
+  results_ls <- extract_fpca(results)
+  results_ls$Y <- tf_vec
+  results_ls$model_var <- Y
+  results_ls$method = method_name
+
+  return(results_ls)
 }
