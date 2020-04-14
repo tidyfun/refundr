89503cbacf244463b2a875ecae1a2f240d00af47
Jeff Goldsmith <jeff.goldsmith@columbia.edu>
2020-04-14 12:28:52 -0400

    fleshing out `predict.rfr_fpca` -- works if a new `tf` object is passed in `newdata`

--- R/fpca-methods.R
+++ R/fpca-methods.R
@@ -31,8 +31,8 @@ fitted.rfr_fpca <- function(object, ...){
 #' produces predictions given a new set of values or the original values used for the
 #' model fit.
 #'
-##' @param object an \code{rf_fpca} object
-##' @param newdata An optional `tfd` data vector. If omitted, the fitted values are returned.
+##' @param object an \code{rfr_fpca} object
+##' @param newdata An optional `tf` data vector. If omitted, the fitted values are returned.
 ##' @param ... optional arguments to be passed to methods
 ##'
 ##' @importFrom stats fitted fitted.values
@@ -42,9 +42,31 @@ predict.rfr_fpca <- function(object, newdata, ...){
   ## need different behavior for regular vs. irregular objects?
    # nah, just need to make sure it works for irregular objects.
     # default behavior (with no new data) is to return fitted values
-  if(missing(newdata) || is.null(newdata)) {
-    fitted(object)
+  if (missing(newdata) || is.null(newdata)) {
+    return(fitted(object))
   }
 
+  ## include some data checks -- args for new data and fpc expansion, etc
+
+  Ypred = as.matrix(spread(as.data.frame(newdata), key = .data$arg, value = .data$value)[,-1])
+
+  new_scores = estimate_fpc_scores(object, Ypred)
+
+  coef_list <- split(cbind(1, new_scores), row(cbind(1, new_scores)))
+
+  tfb_ob = object$Yhat_tfb
+
+  structure(
+    coef_list,
+    domain = attr(tfb_ob, "domain"),
+    basis = attr(tfb_ob, "basis"),
+    basis_label = attr(tfb_ob, "basis_label"),
+    basis_matrix = attr(tfb_ob, "basis_matrix"),
+    arg = attr(tfb_ob, "arg"),
+    resolution = attr(tfb_ob, "resolution"),
+    score_variance = attr(tfb_ob, "score_variance"),
+    error_variance = attr(tfb_ob, "error_variance"),
+    class = c("tfb_fpc", "tfb", "tf")
+  )
 
 }
