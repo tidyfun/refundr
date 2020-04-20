41d6307af3bd0d566af227b76b62e07c99a3b309
Jeff Goldsmith <jeff.goldsmith@columbia.edu>
2020-04-19 21:04:11 -0400

    adding rlang dependency

--- R/rfr_fpca.tfd_irreg.R
+++ R/rfr_fpca.tfd_irreg.R
@@ -1,8 +1,9 @@
 ##' @describeIn rfr_fpca FPCA for data on irregular grids defaults to [refund::fpca.sc()]
+##' @importFrom rlang as_name enquo
 ##' @export
 rfr_fpca.tfd_irreg <- function(Y, data, pve = 0.99, npc = NULL, method = fpca_sc, ...){
 
-  method_name = rlang::as_name(enquo(method))
+  method_name = as_name(enquo(method))
 
   tf_vec <- data[[Y]]
   results <- tfb_fpc(tf_vec, method = method, pve = pve, npc = npc, ...)
--- R/rfr_fpca.tfd_reg.R
+++ R/rfr_fpca.tfd_reg.R
@@ -1,8 +1,9 @@
 ##' @describeIn rfr_fpca FPCA for data on a regular grid defaults to [refund::fpca.face()]
+##' @importFrom rlang as_name enquo
 ##' @export
 rfr_fpca.tfd_reg <- function(Y, data, pve = 0.99, npc = NULL, method = fpca_face, ...){
 
-  method_name = rlang::as_name(enquo(method))
+  method_name = as_name(enquo(method))
 
   tf_vec <- data[[Y]]
   results <- tfb_fpc(tf_vec, method = method, pve = pve, npc = npc, ...)
